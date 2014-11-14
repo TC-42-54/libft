bcc2636ac062fa64c544cf9ce9735dfcd68fea0e
Thomas CHEZIERES <tchezier@e2r13p5.42.fr>
2014-11-14 12:02:14 +0100

    kv

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,31 +6,25 @@
 /*   By: tchezier <tchezier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 12:53:19 by tchezier          #+#    #+#             */
-/*   Updated: 2014/11/06 15:44:43 by tchezier         ###   ########.fr       */
+/*   Updated: 2014/11/14 11:24:06 by tchezier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(void *s, int c, size_t n)
+void		*ft_memchr(void *s, int c, size_t n)
 {
-	size_t i;
-	char *r;
-	char *cc;
+	char	*r;
 
 	r = (char *)s;
-	cc = malloc((ft_strlen((const char *)c) + 1) * (sizeof (c)));
-	cc = (char *)c; 
-	i = 0;
 	if (r != NULL)
 	{
-		while ((i < n) && (r[i] != c))
+		while (n--)
 		{
-			i++;
-			if (((unsigned char *)r + i) == c)
-				return (((unsigned char *)s + i));
+			if (*r == c)
+				return (((unsigned char *)r));
+			r++;
 		}
 	}
-	ft_putstr("on est sortis du while !");
 	return (NULL);
 }
